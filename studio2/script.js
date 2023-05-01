dd35b4c2340e64f8a88b2c46a68856d33b11a1fb
Erica Ng <erng@ucdavis.edu>
2023-05-01 13:03:29 -0700

    check yourself finished

--- studio2/script.js
+++ studio2/script.js
@@ -22,39 +22,80 @@ function showSongInfo(point, data) {
 document.querySelector('#changebutton').addEventListener('click', playClick);
 
 function playClick() {
+    // change styling of control elements
     document.querySelector('#changebutton').removeEventListener('click', playClick);
     document.querySelector('#changebutton').className = "fa-solid fa-pause-circle";
-
-    startedSongs = true;
-    if (startedSongs === true) {
-        showSongInfo(0, globalData);
-        startedSongs = false;
-    }
-
+    document.querySelector('#forwardbutton').style.color = 'white';
+    document.querySelector('#backbutton').style.color = 'gray';
+    document.querySelector('#timebar').style.visibility = 'visible';
     // mouse click interaction added to change between data points
     document.querySelector('#forwardbutton').addEventListener('click', forwardClick);
     document.querySelector('#backbutton').addEventListener('click', backwardClick);
+    
+    startedSongs = true;
+    if (startedSongs === true) {
+        startedSongs = false;
+        showSongInfo(0, globalData);
+    }
 
+    document.querySelector('#result').className = 'textappear';
+    setTimeout(removeResultClass, 1000);
+
+    document.querySelector('#timebar').className = 'timecount';
+    
     // playing car driving sound
     carSound.play();
     carSound.loop = true;
 }
 
 function forwardClick(event) {
+    event.preventDefault;
+
     goNextSong = Math.floor(prevClick + 1);
 
     if (goNextSong !== prevClick) {
         showSongInfo(goNextSong, globalData);
         prevClick = goNextSong;
     }
+
+    if (prevClick === 9) {
+        document.querySelector('#forwardbutton').style.color = 'gray';
+    }
+    document.querySelector('#backbutton').style.color = 'white';
+
+    document.querySelector('#result').className = 'textappear';
+    setTimeout(removeResultClass, 1000);
+    changeTimeBarClass();
 }
 
 function backwardClick(event) {
     goPrevSong = Math.floor(prevClick - 1);
+
     if (goPrevSong !== prevClick) {
         showSongInfo(goPrevSong, globalData);
         prevClick = goPrevSong;
     }
+
+    if (prevClick === 0) {
+        document.querySelector('#backbutton').style.color = 'gray';
+    }
+    document.querySelector('#forwardbutton').style.color = 'white';
+
+    document.querySelector('#result').className = 'textappear';
+    setTimeout(removeResultClass, 1000);
+    changeTimeBarClass();
+}
+
+function removeResultClass(event) {
+    document.querySelector('#result').classList.remove('textappear');
+}
+
+function changeTimeBarClass(event) {
+    document.querySelector('#timebar').classList.remove('timecount');
+
+    void document.querySelector('#timebar').offsetWidth;
+
+    document.querySelector('#timebar').classList.add('timecount');
 }
 
 getData();
