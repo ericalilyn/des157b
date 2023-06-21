52a124cc96d0b0536e8e10288ce26ff0ef14ac6b
Erica Ng <erng@ucdavis.edu>
2023-06-21 14:17:36 -0700

    version 3 home page link fixed

--- capstone/development/version3/location/location.js
+++ capstone/development/version3/location/location.js
@@ -27,7 +27,7 @@ Parse.serverURL = 'https://parseapi.back4app.com/';
     // direct to main page
     navIcon.addEventListener('click', function(event) {
         event.preventDefault();
-        window.location.href = 'https://ericawithac.github.io/des157b/capstone/development/version2/index.html';
+        window.location.href = 'https://ericawithac.github.io/des157b/capstone/development/version3/index.html';
     });
 
     // show location gallery section
